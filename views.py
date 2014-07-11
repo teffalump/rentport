20cc9b26c2202c0803d22cddfa0f4c4da26012e5
teffalump <teffalump@riseup.net>
2014-07-11 16:33:09 -0700

    revert mostly

--- views.py
+++ views.py
@@ -633,11 +633,12 @@ def show_payment(pay_id):
         return jsonify({'error': 'No payment with that id'})
     try:
         p = stripe.Charge.retrieve(payment.pay_id,
-                #api_key=payment.from_user.stripe.access_token)
-                api_key=current_app.config['STRIPE_CONSUMER_SECRET'])
+                api_key=payment.from_user.stripe.access_token)
+        if not p:
+            return jsonify({'error': 'No such charge'})
         m = p.to_dict()
     except Exception as inst:
-        return jsonify({'error': 'Error retrieving payment', 'e': inst, 't': type(inst)})
+        return jsonify({'error': 'Error retrieving payment'})
 
     return jsonify({k:v for (k,v) in m.items() if k in \
             ['amount', 'currency', 'paid', 'refunded', 'description']})
