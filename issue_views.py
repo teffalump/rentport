01ee3e42969862b4212f23ed2143ebb9547baafe
teffalump <teffalump@riseup.net>
2014-08-06 19:17:50 -0700

    bug fix

--- issue_views.py
+++ issue_views.py
@@ -54,6 +54,19 @@ def provider_issue_email(work_order):
     return t
 #### /EMAIL STRINGS ####
 
+#### UTILS ####
+def get_url(endpoint, **kw):
+    '''Return endpoint url, or next arg url'''
+    try:
+        return request.args['next']
+    except:
+        return url_for(endpoint, **kw)
+
+def allowed_file(filename):
+    return '.' in filename and \
+       filename.rsplit('.', 1)[1] in current_app.config['ALLOWED_EXTENSIONS']
+#### /UTILS ####
+
 #### Blueprint ####
 rp = Blueprint('issue', __name__, template_folder = 'templates/issue', static_folder='static')
 #### /Blueprint ####
