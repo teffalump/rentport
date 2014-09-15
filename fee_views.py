01b41895559d72029d1a2aafcadb8d5997c64873
teffalump <teffalump@riseup.net>
2014-09-14 19:35:34 -0700

    fix: fee url routing endpoint

--- fee_views.py
+++ fee_views.py
@@ -27,22 +27,25 @@ import stripe
 rp = Blueprint('fee', __name__, template_folder = 'templates/fee', static_folder='static')
 #### /Blueprint ####
 
-
 #### FEES ####
-@rp.route('/fees', methods=['GET'])
-@rp.route('/fees/<int(min=1):page>', methods=['GET'])
-@rp.route('/fees/<int(min=1):page>/<int(min=1):per_page>', methods=['GET'])
+@rp.route('/fee/<int:pay_id>/show', methods=['GET'])
 @login_required
-def fees(page=1, per_page=current_app.config['PAYMENTS_PER_PAGE']):
-    '''main fees page
-        params:     GET: <page> what page to show
-                    GET: <per_page> how many items per page
-        returns:    GET: template'''
-    fees=g.user.fees.paginate(page, per_page, False)
-    return render_template('fees.html', fees=fees)
+def show_fee(pay_id):
+    '''show extended fee info
+        params:     GET: <pay_id> payment id
+        returns:    GET: json-ed payment info'''
+    fee=g.user.fees.filter(Fee.id==pay_id).first()
+    if not fee:
+        flash('No fee with that id')
+        return jsonify({'error': 'No fee with that id'})
+    f = stripe.Charge.retrieve(fee.pay_id,
+                    api_key=current_app.config['STRIPE_CONSUMER_SECRET'])\
+                    .to_dict()
+    return jsonify({k:v for (k,v) in f.items() if k in \
+            ['amount', 'currency', 'paid', 'refunded','description']})
 
 # RISK
-@rp.route('/fees/pay', methods=['POST', 'GET'])
+@rp.route('/fee/pay', methods=['POST', 'GET'])
 @login_required
 def pay_fee():
     if request.method == 'POST':
@@ -70,26 +73,22 @@ def pay_fee():
         except Exception:
             flash('Other payment error')
         finally:
-            return redirect(url_for('rentport.fees'))
+            return redirect(url_for('fee.fees'))
 
     else:
         return render_template('pay_service_fee.html',
                                 amount=current_app.config['FEE_AMOUNT'],
                                 key=current_app.config['STRIPE_CONSUMER_KEY'])
 
-@rp.route('/fees/<int:pay_id>/show', methods=['GET'])
+@rp.route('/fee/<int(min=1):page>/<int(min=1):per_page>', methods=['GET'])
+@rp.route('/fee/<int(min=1):page>', defaults={'per_page': current_app.config['PAYMENTS_PER_PAGE']}, methods=['GET'])
+@rp.route('/fee', defaults={'page':1, 'per_page': current_app.config['PAYMENTS_PER_PAGE']}, methods=['GET'])
 @login_required
-def show_fee(pay_id):
-    '''show extended fee info
-        params:     GET: <pay_id> payment id
-        returns:    GET: json-ed payment info'''
-    fee=g.user.fees.filter(Fee.id==pay_id).first()
-    if not fee:
-        flash('No fee with that id')
-        return jsonify({'error': 'No fee with that id'})
-    f = stripe.Charge.retrieve(fee.pay_id,
-                    api_key=current_app.config['STRIPE_CONSUMER_SECRET'])\
-                    .to_dict()
-    return jsonify({k:v for (k,v) in f.items() if k in \
-            ['amount', 'currency', 'paid', 'refunded','description']})
+def fees(page, per_page):
+    '''main fees page
+        params:     GET: <page> what page to show
+                    GET: <per_page> how many items per page
+        returns:    GET: template'''
+    fees=g.user.fees.paginate(page, per_page, False)
+    return render_template('fees.html', fees=fees)
 #### /FEES ####
