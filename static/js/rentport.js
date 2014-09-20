56bcc46f4246ebb53f89808b83e925407127b92d
teffalump <teffalump@riseup.net>
2014-09-19 23:54:09 -0700

    hopefully fix binding

--- static/js/rentport.js
+++ static/js/rentport.js
@@ -1,58 +1,60 @@
-$( "#comment_form" ).on('submit', function() {
-    event.preventDefault();
-    var $form = $( this ),
-        url=$form.attr("action"),
-        data=$form.serialize();
-    var r = $.post(url, data);
-    r.done(function(data) {
-        if (data.hasOwnProperty('success')) {
-            line = "<li class='list-group-item'>@"+data.username+" ("+data.time+"): "+data.comment;
-            $( "#comments" ).append(line);
-        } else {
-          line='<div class="alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>'+data.error+'</strong></div>';
-            $( '#messages').append(line);}
-        });
-    $('#comment').val('');
-});
+$(document).ready(function() {
+    $(document).on('submit','#comment_form', function() {
+        event.preventDefault();
+        var $form = $( this ),
+            url=$form.attr("action"),
+            data=$form.serialize();
+        var r = $.post(url, data);
+        r.done(function(data) {
+            if (data.hasOwnProperty('success')) {
+                line = "<li class='list-group-item'>@"+data.username+" ("+data.time+"): "+data.comment;
+                $( "#comments" ).append(line);
+            } else {
+              line='<div class="alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>'+data.error+'</strong></div>';
+                $( '#messages').append(line);}
+            });
+        $('#comment').val('');
+    });
 
-String.prototype.decodeHTML = function() {
-return $("<div>", {html: "" + this}).html();
-};
-var $main = $("#main"),
-ajaxLoad = function(html) {
-document.title = html
-  .match(/<title>(.*?)<\/title>/)[1]
-  .trim()
-  .decodeHTML();
-},
-loadPage = function(href) {
-$main.load(href + " #main>*", ajaxLoad);
-};
+      String.prototype.decodeHTML = function() {
+        return $("<div>", {html: "" + this}).html();
+      };
+      var $main = $("#main"),
+      ajaxLoad = function(html) {
+        document.title = html
+          .match(/<title>(.*?)<\/title>/)[1]
+          .trim()
+          .decodeHTML();
+      },
+      loadPage = function(href) {
+        $main.load(href + " #main>*", ajaxLoad);
+      };
 
-//Back button to work
-$(window).on("popstate", function(e) {
-if (e.originalEvent.state !== null) {
-  loadPage(location.href);
-}
-});
+      //Back button to work
+      $(window).on("popstate", function(e) {
+        if (e.originalEvent.state !== null) {
+          loadPage(location.href);
+        }
+      });
 
-//Select all links except the dropdown buttons, logout button, service
-//fee
-$(document).on("click", "a:not(.dropdown-toggle):not(#logout):not(#serviceFee), area, .clickableRow", function() {
-var href = $(this).attr("href");
+      //Select all links except the dropdown buttons, logout button, service
+      //fee
+      $(document).on("click", "a:not(.dropdown-toggle):not(#logout):not(#serviceFee), area, .clickableRow", function() {
+        var href = $(this).attr("href");
 
-if (href.indexOf(document.domain) > -1
-  || href.indexOf(':') === -1)
-{
-  history.pushState({id: href}, '', href);
-  loadPage(href);
-  // Since ajax, initiate dropdown toggle (to hide)
-  $('.dropdown.open .dropdown-toggle').dropdown('toggle');
-  // Remove old active class
-  //$(this).parent('li').siblings( ".active" ).removeClass('active');
-  $('li .active').removeClass('active');
-  // Add new active class
-  $(this).parent('li').addClass('active');
-  return false;
-}
+        if (href.indexOf(document.domain) > -1
+          || href.indexOf(':') === -1)
+        {
+          history.pushState({id: href}, '', href);
+          loadPage(href);
+          // Since ajax, initiate dropdown toggle (to hide)
+          $('.dropdown.open .dropdown-toggle').dropdown('toggle');
+          // Remove old active class
+          //$(this).parent('li').siblings( ".active" ).removeClass('active');
+          $('li .active').removeClass('active');
+          // Add new active class
+          $(this).parent('li').addClass('active');
+          return false;
+        }
+      });
 });
