ce438f5fda2e3174807938634d805a197f21826f
teffalump <teffalump@riseup.net>
2015-02-07 17:42:12 -0800

    Add pwstrength plugin; update fields

--- static/js/rentport.js
+++ static/js/rentport.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+        "use strict";
+        var options = {};
+        options.ui = {
+            container: "#pwd-container",
+            viewports: {
+                progress: ".pwstrength_viewport_progress",
+                verdict: ".pwstrength_viewport_verdict"
+            }
+        };
+        options.common = {
+            onLoad: function () {
+                $('#messages').text('Start typing password');
+            },
+            zxcvbn: true,
+        };
+        $(':password').pwstrength(options);
+        });
     //$('#main').on('submit','#comment_form', function() {
         //event.preventDefault();
         //var $form = $( this ),
