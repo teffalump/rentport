4988578893de3da79237e6e6880f3c274ce3ca35
teffalump <teffalump@riseup.net>
2015-04-11 17:39:46 -0700

    Fix custom provider workflow; other fixes

--- views/provider.py
+++ views/provider.py
@@ -30,7 +30,7 @@ provider = Blueprint('provider', __name__, template_folder = '../templates/provi
 #### EMAIL STRINGS ####
 def provider_issue_email(work_order):
     email='The landlord has picked a provider. Discuss with your landlord about fixing the issue - {0}:\n\nName: {1}\n\nEmail: {2}\n\nPhone: {3}\n\nWebsite: {4}'
-    if work_order.provider.yelp_id:
+    if isinstance(work_order.provider, YelpProvider):
         info=get_yelp_business_info(work_order.provider.yelp_id)
         name=info['name']
         email='<unknown>'
@@ -43,7 +43,7 @@ def provider_issue_email(work_order):
         name=work_order.provider.name
 
     t = email.format(
-            url_for('.show_issue', ident=work_order.issue.id, _external=True),
+            url_for('issue.show_issue', ident=work_order.issue.id, _external=True),
             name,
             email,
             phone,
@@ -72,6 +72,7 @@ def select_provider(ident):
         return redirect_xhr_or_normal('issue.issues')
     return render_xhr_or_normal('select_provider.html',
                     prev_url=url_for('.saved_providers', ident=issue.id),
+                    cust_url=url_for('.add_local_provider', next=url_for('.saved_providers', ident=ident)),
                     yelp_url=url_for('.yelp_providers', ident=issue.id))
 
 @provider.route('/issues/<int(min=1):ident>/provider/yelp', methods=['GET', 'POST'])
@@ -132,10 +133,10 @@ def saved_providers(ident):
                 Issue.id == ident).first()
     if not issue:
         flash('Not an open issue')
-        return redirect_xhr_or_normal('.show_issue', ident=ident)
+        return redirect_xhr_or_normal('issue.show_issue', ident=ident)
     if issue.work_orders.first():
         flash('Provider already selected')
-        return redirect_xhr_or_normal('.show_issue', ident=ident)
+        return redirect_xhr_or_normal('issue.show_issue', ident=ident)
     form=SelectProviderForm()
     ps = [(str(prov.id), prov.name) for prov in g.user.providers]
     if not ps:
@@ -161,6 +162,7 @@ def saved_providers(ident):
 def add_local_provider():
     '''Add local provider'''
     form=AddProviderForm()
+    n = request.args.get('next')
     if form.validate_on_submit():
         p=SavedProvider()
         p.service=request.form.get('area')
@@ -171,9 +173,9 @@ def add_local_provider():
         p.by_user_id=g.user.id
         db.session.add(p)
         db.session.commit()
-        flash('Provider added')
-        return render_xhr_or_normal('show_provider.html', prov=p)
-    return render_xhr_or_normal('add_provider.html', form=form)
+        flash('Saved provider information')
+        return redirect_xhr_or_normal('.show_providers', prov_id=p.id) 
+    return render_xhr_or_normal('add_provider.html', form=form, next=n)
 
 @provider.route('/landlord/provider/yelp', methods=['GET', 'POST'])
 @login_required
@@ -224,7 +226,7 @@ def show_providers(prov_id):
         if not x:
             flash('Not a valid provider')
             return redirect_xhr_or_normal('.show_providers')
-        if x.yelp_id:
+        if isinstance(x, YelpProvider):
             i = get_yelp_business_info(x.yelp_id)
             x.website = i['url']
             x.phone = i['phone']
